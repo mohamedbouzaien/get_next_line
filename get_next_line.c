f2a5041f97634999563b309c9d5eb4472a54106a
Mohamed BOUZAIEN <mbouzaie@e1r4p6.42.fr>
2019-03-06 20:29:34 +0100

    first commit hope last

--- get_next_line.c
+++ get_next_line.c
@@ -6,13 +6,14 @@
 /*   By: mbouzaie <mbouzaie@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/19 13:54:38 by mbouzaie          #+#    #+#             */
-/*   Updated: 2019/02/11 20:23:57 by mbouzaie         ###   ########.fr       */
+/*   Updated: 2019/03/05 19:54:19 by mbouzaie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 #include "libft/libft.h"
-size_t	chrloc(char *str, int ch)
+
+int			chrloc(char *str, int ch)
 {
 	int i;
 
@@ -23,40 +24,76 @@ size_t	chrloc(char *str, int ch)
 	}
 	if (str[i] == ch)
 		return (i);
-	return (0);
+	return (-1);
 }
-char	*ft_strjointillbsn(char *line, char *buff)
+
+char		*ft_strjointillbsn(char *line, char *buff)
 {
-	char *str;
-	size_t lebbuff;
+	char	*str;
+	int		lebbuff;
 
 	lebbuff = chrloc(buff, '\n');
-	if (lebbuff == 0)
+	if (lebbuff == -1)
 		lebbuff = ft_strlen(buff);
 	if (!(str = ft_strnew(ft_strlen(line) + lebbuff)))
 		return (NULL);
 	ft_strcpy(str, line);
-	ft_strlcat(str, buff, lebbuff);
+	ft_strlcat(str, buff, ft_strlen(line) + lebbuff + 1);
 	return (str);
 }
 
-int	get_next_line(const int fd, char **line)
+char		*get_rest_buffer(char *buff)
 {
-	char	buff[BUFF_SIZE + 1];
-	size_t	errororsize;
+	int			i;
+	char		*p;
+	char		*str;
+
+	i = 0;
+	p = NULL;
+	while (i <= BUFF_SIZE)
+	{
+		if ((i < BUFF_SIZE) && (buff[i] == '\n') && buff[i + 1] != '\n')
+		{
+			p = &buff[i + 1];
+			break ;
+		}
+		i++;
+	}
+	if (p != NULL)
+	{
+		i = ft_strlen(p);
+		str = ft_strnew(i);
+		ft_strcpy(str, p);
+		ft_strclr(buff);
+		return (str);
+	}
+	return (NULL);
+}
+
+int			get_next_line(const int fd, char **line)
+{
+	char		buff[BUFF_SIZE + 1];
+	int			errororsize;
 	static char *str;
-	
+
 	if (fd < 0 || line == NULL || read(fd, buff, 0) < 0)
 		return (-1);
+	ft_memset(buff, '\0', BUFF_SIZE);
 	MALLCHECK((*line = ft_strnew(1)));
+	if (str != NULL)
+	{
+		MALLCHECK((*line = ft_strjointillbsn(*line, str)));
+		str = get_rest_buffer(str);
+	}
 	while ((errororsize = read(fd, buff, BUFF_SIZE)))
 	{
-		buff[errororsize] = '\0';
-		MALLCHECK(( *line = ft_strjointillbsn(*line, buff)));
-		if ((str = ft_strchr(buff, '\n')))
+		MALLCHECK((*line = ft_strjointillbsn(*line, buff)));
+		if ((ft_strchr(buff, '\n')))
+		{
+			str = get_rest_buffer(buff);
 			break ;
+		}
+		ft_strclr(buff);
 	}
-	if (errororsize < BUFF_SIZE)
-		return (0);
-	return (1);
+	return ((errororsize < BUFF_SIZE && !ft_strlen(*line)) ? 0 : 1);
 }
